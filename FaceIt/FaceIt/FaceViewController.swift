b8dadef75b688c582caff4a7bb33d309629cc0f4
robertwang <robertwang.tw@gmail.com>
2016-10-30 00:55:14 +0800

    Add FaceIt project

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Bo Wen Wang on 2016/10/29.
+//  Copyright © 2016年 Bo Wen Wang. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+ 
+
+}
+
